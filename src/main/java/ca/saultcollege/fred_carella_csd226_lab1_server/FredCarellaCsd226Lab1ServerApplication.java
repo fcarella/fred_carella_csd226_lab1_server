da5ca7d9b3b67c17e70556b51d0c224887e7cd2c
fcarella <fred.carella@gmail.com>
2024-01-11 11:51:46 -0500

    Initial commit

---
+++ src/main/java/ca/saultcollege/fred_carella_csd226_lab1_server/FredCarellaCsd226Lab1ServerApplication.java
@@ -0,0 +1,13 @@
+package ca.saultcollege.fred_carella_csd226_lab1_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FredCarellaCsd226Lab1ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FredCarellaCsd226Lab1ServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ca/saultcollege/fred_carella_csd226_lab1_server/FredCarellaCsd226Lab1ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.saultcollege.fred_carella_csd226_lab1_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FredCarellaCsd226Lab1ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
