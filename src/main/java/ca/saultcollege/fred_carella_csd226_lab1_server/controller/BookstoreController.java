e908f4618445fffa8d987160835f2cfb2a852eee
fcarella <fred.carella@gmail.com>
2024-01-11 15:28:45 -0500

    commit

--- src/main/java/ca/saultcollege/fred_carella_csd226_lab1_server/controller/BookstoreController.java
+++ src/main/java/ca/saultcollege/fred_carella_csd226_lab1_server/controller/BookstoreController.java
@@ -1,6 +1,7 @@
 package ca.saultcollege.fred_carella_csd226_lab1_server.controller;
 
 import ca.saultcollege.fred_carella_csd226_lab1_server.data.Account;
+import ca.saultcollege.fred_carella_csd226_lab1_server.data.Content;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,6 +12,10 @@ public class BookstoreController {
     @GetMapping("/publiccontent")
     public ResponseEntity<String> getPublicContent(){ // map a URL to a method
         return ResponseEntity.ok("getPublicContent() :");
+    }
+    @GetMapping("/publiccontent2")
+    public Content getPublicContent2(){ // map a URL to a method
+        return new Content("some content");
     }
 
 //    @PostMapping("/signup")
